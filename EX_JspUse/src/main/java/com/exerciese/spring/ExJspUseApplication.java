a85218ccebeaa2586a724a9da4df53f66700fe4e
superssol <hansole10@gmail.com>
2022-11-08 09:45:23 +0900

    first commit

---
+++ EX_JspUse/src/main/java/com/exerciese/spring/ExJspUseApplication.java
@@ -0,0 +1,13 @@
+package com.exerciese.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExJspUseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExJspUseApplication.class, args);
+	}
+
+}
